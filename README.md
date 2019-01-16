a728cdcf9c9a8e8482efe384732d41520105da2f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-01-15 22:08:47 -0800

    Add full demo

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Contents:
 
 * [Azure setup](#azure-setup)
   * [Get your Azure account](#get-your-azure-account)
-  * [Get Azure software](#get-azure-software)
+  * [Get Azure command line software (optional)](#get-azure-command-line-software-optional)
   * [Get your Azure subscription id](#get-your-azure-subscription-id)
+  * [Sign in (optional)](#sign-in-optional)
 * [Terraform setup](#terraform-setup)
   * [Install](#install)
   * [Configure](#configure)
+  * [Initialize](#initialize)
   * [Build](#build)
+  * [Plan](#plan)
+  * [Congratulations](#congratulations)
 
 
 ## Azure setup
@@ -25,16 +29,32 @@ Get an Azure account, if you don't already have one:
 
   * Sign up.
   
-  * You'll need a credit card.
+Azure offers a free trial period; this is fine for this demo. When we did a free trial period, Azure required a credit card.
+
+Azure offers Pay-As-You-Go signup; this is what we use. If you use Pay-As-You-Go, remember to delete resources when you're done with them.
 
 
-### Get Azure software
+### Get Azure command line software (optional)
 
-If you want, you can install Python, PIP, and Azure on macOS:
+To install Azure CLI on macOS via brew:
 
-    brew install python
-    pip install --pre azure
+```sh
+$ brew update && brew install azure-cli
+```
 
+To install Azure CLI via python pip:
+
+```sh
+$ pip install azure --upgrade --user
+```
+
+Verify:
+
+```sh
+$ az --version
+azure-cli (2.0.33)
+...
+```
 
 ### Get your Azure subscription id
 
@@ -44,12 +64,44 @@ Get your Azure subscription id:
 
   * To see your subscription id, look on the web page left side icon column.
 
-  * click the "Billing" icon. 
+  * Click the item "Cost Management + Billing".
 
   * You see "Active subscriptions you've created" and a column "SUBSCRIPTION ID" column. 
 
   * Make a note of your subscription id.
 
+
+### Sign in (optional)
+
+To use the Azure CLI to sign in:
+
+```sh
+$ az login
+To sign in, use a web browser to open the page
+https://microsoft.com/devicelogin and enter the
+code NGYE5M41LJ18224T to authenticate.
+```
+
+Successful sign in will reply with JSON.
+
+Example:
+
+```json
+[
+  {
+    "cloudName": "AzureCloud",
+    "id": "244a3a0d-ff3b-b935-ecba-bc490ee73b1d",
+    "isDefault": true,
+    "name": "Pay-As-You-Go",
+    "state": "Enabled",
+    "tenantId": "e44cb508-53fa-edf5-93b1-9df17410e137",
+    "user": {
+      "name": "alice@example.com",
+      "type": "user"
+    }
+  }
+]
+```
 
 ## Terraform setup
 
@@ -60,10 +112,6 @@ Use the Terraform install page.
 
   * Go to https://www.terraform.io/intro/getting-started/install.html
 
-  * Caveat: When I try to install Terraform on a MacBook with macOS by using `brew install terraform`, then the brew tool warns me that this is not yet supported. 
-
-  * Workaround on Mac: download the Mac binary, unzip it, and move the `terraform` binary to somewhere convenient; I move it to my `/usr/local/bin` directory, which already on my path.
-
 
 ### Configure
 
@@ -71,6 +119,19 @@ Create a Terraform configuration file.
 
 Our demo configuration file is [demo.tf](demo.tf)
 
+
+### Initialize
+
+Initialize Terraform for the Azure Provider:
+
+```sh
+$ terraform init
+Initializing provider plugins...
+- Checking for available provider plugins on https://releases.hashicorp.com...
+- Downloading plugin for provider "azurerm" (1.21.0)...
+...
+Terraform has been successfully initialized!
+```
 
 ### Build
 
@@ -85,5 +146,23 @@ Typical commands:
   * `terraform apply` runs it.
 
   * `terraform show` prints the results file.
+
+
+### Plan
+
+Plan example:
+
+```sh
+$ terraform plan
+Refreshing Terraform state in-memory prior to plan...
+...
+Terraform will perform the following actions:
+...
+Plan: 1 to add, 0 to change, 0 to destroy.
+...
+```
+
+
+### Congratulations
 
 Congratulations, you're up and running!
--- demo.tf
+++ demo.tf
@@ -1,36 +1,180 @@
+##
 # Configure the Microsoft Azure Provider
+#
+# See https://www.terraform.io/docs/providers/azurerm
+##
+
 provider "azurerm" {
-  subscription_id = "..."
-  client_id       = "..."
-  client_secret   = "..."
-  tenant_id       = "..."
+  # Terraform needs to know the Azure credentials:
+  #
+  #   * subscription_id
+  #   * client_id
+  #   * client_secret
+  #   * tenant_id
+  #
+  # Terraform supports a number of different methods for authenticating to Azure:
+  #
+  #   * Using the Azure CLI
+  #   * Using a Managed Service Identity
+  #   * Using a Service Principal and a Client Certificate
+  #   * Using a Service Principal and a Client Secret
+  #
+  # When we run Terraform automatically (such as in a CI server) we recommend
+  # authentication using a Service Principal or Managed Service Identity.
+  #
+  # When we run Terraform interactively (such as on a command line) we recommend
+  # authenticating using the Azure CLI when running Terraform locally.
+  #
+  # Arguments:
+  #
+  #   * client_id - (Optional) The Client ID which should be used.
+  #     This can also be sourced from the ARM_CLIENT_ID Environment Variable.
+  #
+  #   * subscription_id - (Optional) The Subscription ID which should be used.
+  #     This can also be sourced from the ARM_SUBSCRIPTION_ID Environment Variable.
+  #
+  #   * tenant_id - (Optional) The Tenant ID which should be used.
+  #     This can also be sourced from the ARM_TENANT_ID Environment Variable.
+  #
+  #   * environment - (Optional) The Cloud Environment which should be used.
+  #     Possible values are public, usgovernment, german and china. Defaults to public.
+  #     This can also be sourced from the ARM_ENVIRONMENT environment variable.
+  #
+  # When authenticating as a Service Principal using a Client Secret,
+  # the following fields can be set:
+  #
+  #   * client_secret - (Optional) The Client Secret which should be used.
+  #     This can also be sourced from the ARM_CLIENT_SECRET Environment Variable.
+  #
+  # If you choose to put arguments in environment varaiables,
+  # then here's one way to do it:
+  #
+  #     export ARM_SUBSCRIPTION_ID=""
+  #     export ARM_CLIENT_ID=""
+  #     export ARM_CLIENT_SECRET=""
+  #     export ARM_TENANT_ID=""
+  #     export ARM_ENVIRONMENT="public"
+  #
+  # If you choose to put the credentials in this configuration file,
+  # then here are the lines:
+  #
+  #     subscription_id = "..."
+  #     client_id = ""
+  #     client_secret = ""
+  #     tenant_id = ""
+  #     environment="public"
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "production" {
-    name     = "production"
-    location = "West US"
+##
+# Create a resource group.
+#
+# We must do this first, because the resource group
+# is what connects all the rest for the resources.
+##
+
+resource "azurerm_resource_group" "demo" {
+  name = "demo-resource-group"
+  location = "West US"
 }
 
-# Create a virtual network in the web_servers resource group
-resource "azurerm_virtual_network" "network" {
-  name                = "productionNetwork"
-  address_space       = ["10.0.0.0/16"]
-  location            = "West US"
-  resource_group_name = "${azurerm_resource_group.production.name}"
+##
+# Create a virtual network.
+#
+# This needs the resource group.
+# This is is needed by the internal subnet.
+#
+# This example also shows how to create a few subnets at
+# the same time as the virtual network; this is optional.
+# This demo does not do anything with these subnets.
+##
 
+resource "azurerm_virtual_network" "demo" {
+  name = "demo-virtual-network"
+  address_space = ["10.0.0.0/16"]
+  location = "${azurerm_resource_group.demo.location}"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
   subnet {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
   }
-
   subnet {
     name           = "subnet2"
     address_prefix = "10.0.2.0/24"
   }
-
   subnet {
     name           = "subnet3"
     address_prefix = "10.0.3.0/24"
+  }
+}
+
+##
+# Create an internal subnet.
+#
+# This needs the virtual network.
+# This is needd by the network interface.
+##
+
+resource "azurerm_subnet" "internal" {
+  name = "demo-subnet"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
+  virtual_network_name = "${azurerm_virtual_network.demo.name}"
+  address_prefix = "10.0.2.0/24"
+}
+
+##
+# Create a network interface.
+#
+# We must do this before we create any virtual machines,
+# because the virtual machine needs the network interface id.
+##
+
+resource "azurerm_network_interface" "demo" {
+  name = "demo-network-interface"
+  location = "${azurerm_resource_group.demo.location}"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
+  ip_configuration {
+    name = "demo-ip-configuration"
+    subnet_id = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+##
+# Create a virtual machine.
+#
+# For the operating system, we like to demo using Ubuntu server,
+# and with the current Long Term Support (LTS) version.
+##
+
+resource "azurerm_virtual_machine" "demo" {
+  name = "demo-virtual-machine"
+  location = "${azurerm_resource_group.demo.location}"
+  resource_group_name = "${azurerm_resource_group.demo.name}"
+  network_interface_ids = ["${azurerm_network_interface.demo.id}"]
+  vm_size = "Standard_DS1_v2"
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+  storage_image_reference {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "18.04-LTS"
+    version = "latest"
+  }
+  storage_os_disk {
+    name = "myosdisk1"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }  
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "admin"
+    admin_password = "secret"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags {
+    environment = "staging"
   }
 }
